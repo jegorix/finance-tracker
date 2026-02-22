cf22452d7e6cb166c6d0ffede43837ff7e507a65
jegorix <0311egor@gmail.com>
2026-02-22 20:53:02 +0300

    Implemented dto logic and related mapper

---
+++ src/main/java/com/finance/tracker/dto/request/TransactionRequest.java
@@ -0,0 +1,42 @@
+package com.finance.tracker.dto.request;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+public class TransactionRequest {
+
+    private BigDecimal amount;
+    private String category;
+    private LocalDate date;
+
+    public BigDecimal getAmount()
+    {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount)
+    {
+        this.amount = amount;
+    }
+
+    public String getCategory()
+    {
+        return category;
+    }
+
+    public void setCategory(String category)
+    {
+        this.category = category;
+    }
+
+    public LocalDate getDate()
+    {
+        return date;
+    }
+
+    public void setDate(LocalDate date)
+    {
+        this.date = date;
+    }
+
+}
---
+++ src/main/java/com/finance/tracker/mapper/TransactionMapper.java
@@ -0,0 +1,26 @@
+package com.finance.tracker.mapper;
+import com.finance.tracker.dto.request.TransactionRequest;
+import com.finance.tracker.dto.response.TransactionResponse;
+import com.finance.tracker.entity.Transaction;
+
+public class TransactionMapper {
+
+    public static TransactionResponse toResponse(Transaction transaction) {
+
+        TransactionResponse response = new TransactionResponse();
+        response.setId(transaction.getId());
+        response.setAmount(transaction.getAmount());
+        response.setCategory(transaction.getCategory());
+        response.setDate(transaction.getDate());
+        return response;
+    }
+    
+    public static Transaction toEntity(Transaction request) {
+        Transaction transaction = new Transaction();
+        transaction.setAmount(request.getAmount());
+        transaction.setCategory(request.getCategory());
+        transaction.setDate(request.getDate());
+        return transaction;
+    }
+
+}
